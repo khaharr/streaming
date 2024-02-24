7820e0e15632ecd071b870cfd55d684bda43fb54
Younès Bouchouk <115986198+Younes-Bouchouk@users.noreply.github.com>
2024-02-24 23:46:02 +0100

    Nav bar Responsive, problèmes restants : boutons de catégories

--- src/header.php
+++ src/header.php
@@ -11,7 +11,7 @@
         <a href="liste-anime.php">Anime</a>
             <!-- Catégories avec menu déroulant -->
         <div id="categories">
-            <a href="#">Categories <i class="fas fa-caret-down"></i></a></li>
+            <a href="#">Categories <i class="fas fa-caret-down"></i></a>
             <div class="drop-down-box">
                 <a href="#">Action</a>
                 <a href="#">Aventure</a>
@@ -33,4 +33,55 @@
         <button onclick="location.href='connexion.php'" id="signIn">Se Déconnecter</button>
     </div>
 
+    <div id="responsive-menu">
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+
 </div>
+
+<script>
+
+    const nav = document.querySelector('#Navbar>nav'); // Les boutons de navigations
+    const navOptions = document.querySelector('#nav-options'); // Les options de navigations
+    const menu = document.querySelector('#responsive-menu'); // Le menu hamburger
+    let menuHidden = true; // Variable de status du menu
+
+    // Rend visible/invisible les boutons de navigations au clic du menu hamburger
+    menu.addEventListener('click', function(){    
+        if (menuHidden) { // Afficher si le menu est invisible
+            nav.style.display = 'flex';
+            menuHidden = false;
+        } else { // Caché si le menu est visible
+            nav.style.display = 'none';
+            menuHidden = true;
+        }
+    })
+
+    // Rend visible/invisible le menu hamburger et les boutons de navigation
+    function responsiveHeader() {
+
+        if (window.innerWidth < 1000) { // Fenêtre inférieur à 1000px
+
+            if (menuHidden) { // Cache seulement si le menu n'est pas visible
+                nav.style.display = 'none'; // Cache les boutons
+                navOptions.style.display = 'none';  // Cache les options
+            }
+
+            nav.classList.add('responsive-nav') // Style css responsive
+            menu.style.display = 'flex'; // Affiche le menu hamburger
+
+        } else {
+            nav.style.display = 'flex';
+            navOptions.style.display = 'flex';
+            nav.classList.remove('responsive-nav')
+            menu.style.display = 'none';
+        }
+    }
+
+    responsiveHeader(); // Applique la fonction au chargement de la page
+
+    // Applique la fonction au redimmensionnement de la page
+    window.addEventListener('resize', responsiveHeader);
+</script>

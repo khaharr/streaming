1fc27c62d5789e848002060c75d4347649f33371
Khlidbg78dem <khalidjj78200@icloud.com>
2024-04-02 11:21:47 +0200

    list-anime complete avec bdd

---
+++ public/actions/listAnime.php
@@ -0,0 +1,24 @@
+<?php
+include "loginConnexion.php";
+
+// Requête pour récupérer les données de la table anime
+$sql = "SELECT nom, image  FROM anime";
+$result = $conn->query($sql);
+
+// Vérifier s'il y a des résultats
+if ($result->num_rows > 0) {
+    // Parcourir les résultats et les afficher
+    while($row = $result->fetch_assoc()) {
+        echo '<div class="Image-Name">';
+        echo '<img src="' . $row["image"] . '" alt="' . $row["nom"] . '">';
+        echo '<div class="show-name">' . $row["nom"] . '</div>';
+        echo '</div>';
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
+
+
+?>
